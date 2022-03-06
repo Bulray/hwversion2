a5c44ec3c409f018af89f1ea0514dcf6356c51a3
bulra <bulray30@gmail.com>
2022-03-06 19:38:40 +0300

    Initial commit

---
+++ venv/app.py
@@ -0,0 +1,31 @@
+import sqlite3
+from flask import Flask, jsonify
+import queries
+
+app = Flask(__name__)
+
+DATABASE_PATH = 'animal.db'
+
+
+def serialize_row(row):
+    return {key: row[key] for key in row.keys()}
+
+
+@app.route('/<animal_id>')
+def get_animal_by_id(animal_id):
+    conn: sqlite3.Connection = app.config['db']
+    cursor = conn.cursor()
+    cursor.execute(queries.GET_ANIMAL_BY_ID_QUERY, (animal_id,))
+    row = cursor.fetchone()
+    cursor.close()
+    return jsonify(serialize_row(row))
+
+
+if __name__ == '__main__':
+    connection = sqlite3.connect(DATABASE_PATH, check_same_thread=False)
+    connection.row_factory = sqlite3.Row
+    app.config['db'] = connection
+    try:
+        app.run(debug=True)
+    except KeyboardInterrupt:
+        connection.close()
---
+++ venv/queries.py
@@ -0,0 +1,4 @@
+GET_ANIMAL_BY_ID_QUERY = """
+    SELECT * FROM new_animals
+    WHERE animal_id = :1;
+"""
